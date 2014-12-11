ce1aa88eedeab191ffe34667b8439a47d129c8ee
huu tho nguyen <wolflonely76@yahoo.com>
2014-12-11 23:57:03 +0700

    Fix indent

--- app/modules/common.factories.js
+++ app/modules/common.factories.js
@@ -1,27 +1,27 @@
 (function() {
-	'use strict';
-	angular.module('common.factories', [])
+  'use strict';
+  angular.module('common.factories', [])
 
-		/**
-		 * Clone Object
-		 *
-		 * @param  {array|object} will be clone
-		 * @return {array|object} cloned
-		 */
-		.factory('objectUtilities', [function() {
-			var cloneObject = function(object) {
-				return JSON.parse(JSON.stringify(object));
-			}
-			var isArrayEmpty = function(array) {
-				if ( ! array || array.length <= 0) {
-					return true;
-				} else {
-					return false;
-				}
-			}
-			return {
-				'cloneObject': cloneObject,
-				'isArrayEmpty': isArrayEmpty
-			}
-		}]);
+    /**
+     * Clone Object
+     *
+     * @param  {array|object} will be clone
+     * @return {array|object} cloned
+     */
+    .factory('objectUtilities', [function() {
+      var cloneObject = function(object) {
+        return JSON.parse(JSON.stringify(object));
+      }
+      var isArrayEmpty = function(array) {
+        if ( ! array || array.length <= 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      return {
+        'cloneObject': cloneObject,
+        'isArrayEmpty': isArrayEmpty
+      }
+    }]);
 })();
