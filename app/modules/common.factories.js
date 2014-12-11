287f23cfc681a1ef122cbd5b23390375cec0784e
huu tho nguyen <wolflonely76@yahoo.com>
2014-12-12 00:01:46 +0700

    Add comment

--- app/modules/common.factories.js
+++ app/modules/common.factories.js
@@ -3,15 +3,29 @@
   angular.module('common.factories', [])
 
     /**
-     * Clone Object
+     * Object utilities
      *
-     * @param  {array|object} will be clone
-     * @return {array|object} cloned
+     * Useful object's function for handling object 
+     *
      */
     .factory('objectUtilities', [function() {
+
+      /**
+       * Clone Object
+       *
+       * @param  {array|object} will be clone
+       * @return {array|object} cloned
+       */
       var cloneObject = function(object) {
         return JSON.parse(JSON.stringify(object));
       }
+
+      /**
+       * Check array empty
+       *
+       * @param  {array} 
+       * @return {boolean} array is empty --> true, array not empty --> false
+       */
       var isArrayEmpty = function(array) {
         if ( ! array || array.length <= 0) {
           return true;
